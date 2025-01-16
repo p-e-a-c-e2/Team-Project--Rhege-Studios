4c9b3df50c0fb094c8c09f810d960ce8a616e504
p-e-a-c-e2 <smartpeace6@gmail.com>
2025-01-16 22:55:21 +0100

    update

---
+++ script.js
@@ -0,0 +1,34 @@
+
+    document.getElementById('userForm').addEventListener('submit', function(event) {
+        
+        event.preventDefault();
+
+        
+        const formData = {
+            fullname: document.getElementById('fullname').value,
+            email: document.getElementById('email').value,
+            phone: document.getElementById('phone').value,
+            track: document.getElementById('track').value,
+            portfolio: document.getElementById('portfolio').value
+        };
+
+       
+        fetch('https://rhedge-studios.onrender.com/api/v1/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData) 
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);  
+            alert('User information updated successfully!');
+        })
+        .catch((error) => {
+            console.error('Error:', error);  
+            alert('An error occurred. Please try again later.');
+        });
+    });
+
+
